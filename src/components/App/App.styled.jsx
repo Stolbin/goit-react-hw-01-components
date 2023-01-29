c2263a02d7d8f8053062d8d9e7abafaba5ee683f
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-01-29 23:31:35 +0200

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -8,8 +8,6 @@ export const CardContainer = styled.div`
   justify-content: center;
   align-items: center;
   color: #010101;
-  margin-left: auto;
-  margin-right: auto;
 `;
 
 export const CardTex = styled.p`
